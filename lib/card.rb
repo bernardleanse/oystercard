79bf0b185db88092b1d1608adbb10d027ad2a1c3
luke <luke44@gmail.com>
2021-09-27 15:32:49 +0100

    Step 6 complete
    Co-authored-by: Luke <luke44@gmail.com>
    Co-authored-by: Alex <alexboyling@gmail.com>

--- lib/card.rb
+++ lib/card.rb
@@ -1,15 +1,22 @@
-class Oystercard
+# frozen_string_literal: true
 
+class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  MAX_BALANCE = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
   end
 
   def top_up(amount)
+    raise "This exceeds max balance of #{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
+
     @balance += amount
   end
 
+  def at_max?
+    @balance == MAX_BALANCE
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,32 @@
+# frozen_string_literal: true
+
 require './lib/card'
 
+MAX_BALANCE = Oystercard::MAX_BALANCE
+
 describe Oystercard do
+  describe '#balance' do
+    it 'responds to balance' do
+      expect(subject).to respond_to(:balance)
+    end
 
-  it 'responds to balance' do
-    expect(subject).to respond_to(:balance)
+    it 'has a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
   end
 
-  it 'has a default balance of 0' do
-    expect(subject.balance).to eq 0
-  end
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'inceases the balance on the card' do
+      expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+    end
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
-  it 'inceases the balance on the card' do
-    expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
-  end
+    it 'responds to top_up' do
+      expect(subject).to respond_to(:top_up)
+    end
 
+    it 'rejects top ups that exceed the max balance' do
+      expect { subject.top_up(91) }.to raise_error("This exceeds max balance of #{MAX_BALANCE}")
+    end
+  end
 end
