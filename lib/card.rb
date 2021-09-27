365e00663ad9a97a634f762d3b4f15cfb0372e1f
Alex Boyling <alexboyling@gmail.com>
2021-09-27 14:56:11 +0100

    Added top up feature Co-authored-by: Luke <luke44@gmail.com> Co-authored-by: Alex <alexboyling@gmail.com>

--- lib/card.rb
+++ lib/card.rb
@@ -8,4 +8,8 @@ class Oystercard
     @balance = DEFAULT_BALANCE
   end
 
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,4 +10,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+  it 'inceases the balance on the card' do
+    expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+  end
+
 end
